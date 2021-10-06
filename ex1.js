e3ae65fcf0ae173f72ed70a020f3ff5c9ffa95b8
Allen <59067634+allenbronshtein@users.noreply.github.com>
2021-10-06 18:09:29 +0200

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,16 @@
+//	Meni Ashurov 204064257
+//	Ido	Zeira 322607177
+//	Allen Bronshtein 206228751
+function constfuncs() {
+	var funcs = [];
+	for (var i = 0; i < 10; i++)
+		funcs[i] = func
+
+	return funcs;
+}
+
+var func = function () { return i };
+var funcs = constfuncs();
+
+for (var i = 0; i < 10; i++)
+	console.log(funcs[i] + ":", funcs[i]())
